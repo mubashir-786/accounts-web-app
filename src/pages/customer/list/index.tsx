47d36dc30f9e92cc6ca77073d6433f08f24e6151
mubashir raza <60073150+mubashir-786@users.noreply.github.com>
2023-02-21 14:12:19 +0500

    Update index.tsx

--- src/pages/customer/list/index.tsx
+++ src/pages/customer/list/index.tsx
@@ -33,6 +33,16 @@ export default function Page() {
       address: 'Hamitono',
       country: 'USA',
     },
+    {
+      id: '2362',
+      company_name: 'rtr',
+      website: 'www.rtr.com',
+      email: 'rtr@gmail.com',
+      number: '123456789',
+      contact_person: '00123456',
+      address: 'Hamitono',
+      country: 'USA',
+    },
   ];
   return (
     <div>
